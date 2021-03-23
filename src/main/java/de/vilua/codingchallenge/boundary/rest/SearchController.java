5dd24f48dd5a4174b4bcc84ba22efc73294eecea
Md Jahid Hasan <Md-Jahid.Hasan@adesso.de>
2021-03-23 21:20:44 +0100

    implemented search-food-items and search-food controller and Service

--- src/main/java/de/vilua/codingchallenge/boundary/rest/SearchController.java
+++ src/main/java/de/vilua/codingchallenge/boundary/rest/SearchController.java
@@ -1,11 +1,35 @@
 package de.vilua.codingchallenge.boundary.rest;
 
+import com.fatsecret.platform.model.CompactFood;
+import com.fatsecret.platform.model.Food;
+import de.vilua.codingchallenge.control.services.SearchService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/vilua-codingchallenge/v1")
 public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping(path = "/search-food-items", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<CompactFood> searchItems(@RequestParam(value="query") String query) {
+        return this.searchService.searchFoodItems(query);
+    }
+
+    @GetMapping(path = "/search-food", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Food item(@RequestParam(value="id") Long id) {
+        return this.searchService.getFoodItem(id);
+    }
+
 }
--- src/main/java/de/vilua/codingchallenge/control/services/SearchService.java
+++ src/main/java/de/vilua/codingchallenge/control/services/SearchService.java
@@ -1,4 +1,43 @@
 package de.vilua.codingchallenge.control.services;
 
+import com.fatsecret.platform.model.CompactFood;
+import com.fatsecret.platform.model.Food;
+import com.fatsecret.platform.services.FatsecretService;
+import com.fatsecret.platform.services.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SearchService {
+
+    @Value("${fatsecret.apikey}")
+    private String apiKey;
+
+    @Value("${fatsecret.apisecret}")
+    private String apiSecret;
+
+    /**
+     *
+     * @param query ist suchbegriff
+     * Gibt entsprechendes Objekt {@link CompactFood} als Liste zurück
+     */
+    public List<CompactFood> searchFoodItems(String query) {
+        FatsecretService service = new FatsecretService(apiKey, apiSecret);
+        Response<CompactFood> response = service.searchFoods(query);
+        List<CompactFood> results = response.getResults();
+        return results;
+    }
+
+    /**
+     *
+     * @param id ist Food Item ID
+     * Gibt entsprechendes Objekt {@link Food} zurück
+     */
+    public Food getFoodItem(Long id) {
+        FatsecretService service = new FatsecretService(apiKey, apiSecret);
+        return service.getFood(id);
+    }
+
 }
