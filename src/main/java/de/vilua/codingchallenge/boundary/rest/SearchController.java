2b5304d83bbf1d43df58b3132c03ec7465df2526
Md Jahid Hasan <Md-Jahid.Hasan@adesso.de>
2021-03-23 20:55:31 +0100

    added rest controller and service

---
+++ src/main/java/de/vilua/codingchallenge/boundary/rest/SearchController.java
@@ -0,0 +1,11 @@
+package de.vilua.codingchallenge.boundary.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/vilua-codingchallenge/v1")
+public class SearchController {
+}
---
+++ src/main/java/de/vilua/codingchallenge/control/services/SearchService.java
@@ -0,0 +1,4 @@
+package de.vilua.codingchallenge.control.services;
+
+public class SearchService {
+}
